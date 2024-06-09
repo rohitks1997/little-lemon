dcfe055fb81d902d8b7d2d750d3b85305177b22e
Rohit Kumar Setthachok <55288037+rohitks1997@users.noreply.github.com>
2024-06-09 19:38:52 +0700

    Fourth commit

--- little-lemon/src/components/BookingForm.js
+++ little-lemon/src/components/BookingForm.js
@@ -1,7 +1,8 @@
-// src/BookingForm.js
+// src/components/BookingForm.js
 import React from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import './BookingForm.css';
 
 const BookingForm = () => {
   const formik = useFormik({
@@ -30,61 +31,37 @@ const BookingForm = () => {
     <form onSubmit={formik.handleSubmit}>
       <div>
         <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          type="text"
-          {...formik.getFieldProps('name')}
-        />
+        <input id="name" type="text" {...formik.getFieldProps('name')} />
         {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null}
       </div>
 
       <div>
         <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          type="email"
-          {...formik.getFieldProps('email')}
-        />
+        <input id="email" type="email" {...formik.getFieldProps('email')} />
         {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
       </div>
 
       <div>
         <label htmlFor="phone">Phone</label>
-        <input
-          id="phone"
-          type="text"
-          {...formik.getFieldProps('phone')}
-        />
+        <input id="phone" type="text" {...formik.getFieldProps('phone')} />
         {formik.touched.phone && formik.errors.phone ? <div>{formik.errors.phone}</div> : null}
       </div>
 
       <div>
         <label htmlFor="date">Date</label>
-        <input
-          id="date"
-          type="date"
-          {...formik.getFieldProps('date')}
-        />
+        <input id="date" type="date" {...formik.getFieldProps('date')} />
         {formik.touched.date && formik.errors.date ? <div>{formik.errors.date}</div> : null}
       </div>
 
       <div>
         <label htmlFor="time">Time</label>
-        <input
-          id="time"
-          type="time"
-          {...formik.getFieldProps('time')}
-        />
+        <input id="time" type="time" {...formik.getFieldProps('time')} />
         {formik.touched.time && formik.errors.time ? <div>{formik.errors.time}</div> : null}
       </div>
 
       <div>
         <label htmlFor="guests">Number of Guests</label>
-        <input
-          id="guests"
-          type="number"
-          {...formik.getFieldProps('guests')}
-        />
+        <input id="guests" type="number" {...formik.getFieldProps('guests')} />
         {formik.touched.guests && formik.errors.guests ? <div>{formik.errors.guests}</div> : null}
       </div>
 
